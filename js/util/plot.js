678ab7865c1b39294c1c98b66729f56effca4079
cryx3001 <chiko3001@gmail.com>
2020-09-05 02:51:18 +0200

    Don't delete example.png >:(

--- js/util/plot.js
+++ js/util/plot.js
@@ -74,6 +74,7 @@ function getChart(tag, msg) {
 async function deleteCharts() {
 	const dir = await readdir('./img/');
 	dir.forEach((file) => {
+		if(file === "example.png") return;
 		fs.unlink(`img/${file}`, (err) => {
 			if (err) throw err;
 		});
